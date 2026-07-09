ecded98090a121c8ee3312737a427a1ad71e73dc
andrew <andrewalangm@gmail.com>
2026-07-09 17:54:17 -0600

    Setup ash oban with igniner

--- mix.exs
+++ mix.exs
@@ -35,6 +35,8 @@ defmodule Tunez.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:oban, "~> 2.0"},
+      {:ash_oban, "~> 0.8"},
       {:ash_authentication_phoenix, "~> 2.0"},
       {:bcrypt_elixir, "~> 3.0"},
       {:picosat_elixir, "~> 0.2"},
